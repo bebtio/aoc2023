9b4d3012b81712bbc60ed4ac4ea52b1bccd9de4c
bebtio <bebtio@gmail.com>
2023-12-02 16:27:55 -0600

    Added some comments

--- 2023/python/day2/part2.py
+++ 2023/python/day2/part2.py
@@ -6,8 +6,9 @@ class GameInfo:
         self.boxSets = []
         self.isValid = False
         self.power   = 0
-    def parseGame(self, gameFile: str):
-        
+    
+    # Parse the string represent the outcome of this game.
+    def parseGame(self, gameFile: str):    
 
         # Get the gameID
         gameId = gameFile.split(':')[0]
@@ -19,9 +20,11 @@ class GameInfo:
         gameSets = gameFile.split(":")[1]
         gameSets = gameSets.split(";")
 
-
+        # Parse the portion containing game outcomes.
         self.parseGameSets(gameSets)
 
+    # Parse the portion of the game string that contains the sets of
+    # randomly selected colored boxes.
     def parseGameSets(self, sets: str ):
 
         for set in sets:
@@ -40,12 +43,14 @@ class GameInfo:
 
             self.boxSets.append( colorDict )
 
-            
+
+    # Brute force check the sets to see if they invalidate this game.
+    # If any of the *Count input variables are smaller than any sets, number of colored
+    # boxes, this game is invalid.
+    # We could break after this occurs but that's extra logic I don't feel like implementing.            
     def checkIfGameValid(self, redCount:int, greenCount:int, blueCount:int ) -> bool:
 
         valid = False
-
-
         invalidCount = 0
 
         for set in self.boxSets:
@@ -69,19 +74,23 @@ class GameInfo:
 
         return( valid )
 
+    # Get the game points that are stored at self.gameId.
     def getGamePoints(self):
 
         if( self.isValid ):
             return( self.gameId )
         else:
             return(0)
-        
+
+    # Compute the power of the currenct game.        
     def computeGamePower(self) -> int:
 
         minRed   = -1
         minGreen = -1
         minBlue  = -1
 
+        # Loop through the sets and update each color if the currently stored value
+        # is smaller than the one we are looking at.
         for set in self.boxSets:
             for color in set:
                 if color == "red":
@@ -96,29 +105,29 @@ class GameInfo:
                 else:
                     print(f' {color} is not a valid color')
 
+        # compute the power and store it.
         self.power = minRed * minGreen * minBlue
 
         return(self.power)
     
-
+# Parse the input file and compute the power of each game.
 def parseFile( fileName: str ) -> int:
-
-    gameArray = list()
-    rCount = 12
-    gCount = 13
-    bCount = 14
 
     gameSum = 0
 
+    # Opent the file.
     with open(fileName, "r") as f:
 
+        # Get all the lines in the file.
         lines = f.readlines()
 
         for line in lines:
+
+            # Parse each line and create a GameInfo object.
             g = GameInfo()
             g.parseGame(line)
-            
 
+            # Compute the power based on the what was read in.
             gameSum += g.computeGamePower()            
 
             print(f'game {g.gameId}\'s power is {g.power}')
